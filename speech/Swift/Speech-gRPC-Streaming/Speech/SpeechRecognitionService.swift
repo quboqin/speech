178258922470a5b0e8d846cd520fb285666efb95
Qubo Qin <qubo.qin.2018@gmail.com>
2018-10-28 08:36:55 -0400

    fix building problem

--- speech/Swift/Speech-gRPC-Streaming/Speech/SpeechRecognitionService.swift
+++ speech/Swift/Speech-gRPC-Streaming/Speech/SpeechRecognitionService.swift
@@ -16,7 +16,7 @@
 import Foundation
 import googleapis
 
-let API_KEY : String = "YOUR_API_KEY"
+let API_KEY : String = "AIzaSyBZeCnbSasN0Nutu7LkFOYBxf-4OIHFjDA"
 let HOST = "speech.googleapis.com"
 
 typealias SpeechRecognitionCompletionHandler = (StreamingRecognizeResponse?, NSError?) -> (Void)
